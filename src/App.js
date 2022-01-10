9ff95af89972b9af262d6a1dd569a16c7282eec0
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-10 12:26:19 +0000

    added show add button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -45,8 +46,12 @@ function App() {
 
   return (
     <div className='container'>
-      <Header title='Tracker' />
-      <AddTask onAdd={addTask} />
+      <Header
+        title='Tracker '
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
