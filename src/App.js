71de9bb04dff633e6b17eade6719c49cd79e0418
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-10 10:15:08 +0000

    passed task vis props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
-import Tasks from "./components/Task";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Covid Booster",
+      day: "Jan 12th at 3.30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Deliver Packages",
+      day: "Jan 19th at 10.00am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Walk the dog",
+      day: "Jan 31st at 17.30pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className='container'>
       <Header title='Tracker' />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,31 +1,8 @@
-const taskes = [
-  {
-    id: 1,
-    text: "Covid Booster",
-    day: "Jan 12th at 3.30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Deliver Packages",
-    day: "Jan 19th at 10.00am",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Walk the dog",
-    day: "Jan 31st at 17.30pm",
-    reminder: false,
-  },
-];
-
-const Task = () => {
+const Task = ({ task }) => {
   return (
-    <>
-      {taskes.map((task) => (
-        <h3>{task.text}</h3>
-      ))}
-    </>
+    <div className='task'>
+      <h3> {task.text}</h3>
+    </div>
   );
 };
 
