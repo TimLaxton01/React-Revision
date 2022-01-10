8b271ca7431c728e383d05855f59b648fee4cb28
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-10 11:37:22 +0000

    passed reminder into tasks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [tasks, setTask] = useState([
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       text: "Covid Booster",
@@ -25,14 +25,22 @@ function App() {
   ]);
 
   const deleteTask = (id) => {
-    setTask(tasks.filter((task) => task.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className='container'>
       <Header title='Tracker' />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
